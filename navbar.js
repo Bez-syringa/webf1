71321755981472d6992db008d07d3df90da49fd2
Bez-syringa <projektybez@gmail.com>
2024-06-29 18:13:34 +0200

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,2 @@
+document.write 
+('<nav><ul><liclass="menu"></li><li><a href="kontakt.html">Strona glowna</a></li><li><a href="kot.html">Nauka</a></li><li><a href="kontakt.html">Kontakt</a></li></ul></nav>')
